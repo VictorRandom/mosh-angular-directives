aaf95b8cc3b3e5389c0e2bee1d695ae5893401e7
victor.geodata <victor.geodata@gmail.com>
2022-03-21 09:25:01 -0300

    ngFor and trackBy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,14 +18,27 @@ export class AppComponent {
 
   onAdd(){
     this.courses2.push({id:4, name:'course4'})
-  }
+  };
 
   onRemove(course: any){
     let index = this.courses2.indexOf(course);
     this.courses2.splice(index, 1);
-  }
+  };
 
   onChange(course: any){
     course.name = 'Updated';
+  };
+
+  loadCourses(){
+    this.courses2 = [
+      { id: 1, name: 'course1' },
+      { id: 2, name: 'course2' },
+      { id: 3, name: 'course3' }
+    ]
+  };
+
+  trackCourse(index:any, course:any){
+    return course? course.id : undefined;
   }
+
 }
